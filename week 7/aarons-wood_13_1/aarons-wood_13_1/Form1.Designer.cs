1b1bc942b9fb51c1c518a4924e78e2ceab22c991
evan <evanaaronswood@gmail.com>
2024-02-22 13:20:21 -0500

    week 7 done

---
+++ week 7/aarons-wood_13_1/aarons-wood_13_1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace aarons_wood_13_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.integerListBox = new System.Windows.Forms.ListBox();
+            this.negativeBttn = new System.Windows.Forms.Button();
+            this.findallBttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // integerListBox
+            // 
+            this.integerListBox.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.integerListBox.FormattingEnabled = true;
+            this.integerListBox.Location = new System.Drawing.Point(78, 12);
+            this.integerListBox.Name = "integerListBox";
+            this.integerListBox.Size = new System.Drawing.Size(225, 290);
+            this.integerListBox.TabIndex = 0;
+            // 
+            // negativeBttn
+            // 
+            this.negativeBttn.BackColor = System.Drawing.Color.Salmon;
+            this.negativeBttn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.negativeBttn.Location = new System.Drawing.Point(48, 324);
+            this.negativeBttn.Name = "negativeBttn";
+            this.negativeBttn.Size = new System.Drawing.Size(125, 47);
+            this.negativeBttn.TabIndex = 1;
+            this.negativeBttn.Text = "Remove Negative Numbers";
+            this.negativeBttn.UseVisualStyleBackColor = false;
+            this.negativeBttn.Click += new System.EventHandler(this.negativeBttn_Click);
+            // 
+            // findallBttn
+            // 
+            this.findallBttn.BackColor = System.Drawing.Color.Salmon;
+            this.findallBttn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.findallBttn.Location = new System.Drawing.Point(209, 324);
+            this.findallBttn.Name = "findallBttn";
+            this.findallBttn.Size = new System.Drawing.Size(125, 47);
+            this.findallBttn.TabIndex = 2;
+            this.findallBttn.Text = "Display Remaing Values Between 1 - 10";
+            this.findallBttn.UseVisualStyleBackColor = false;
+            this.findallBttn.Click += new System.EventHandler(this.findallBttn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(404, 392);
+            this.Controls.Add(this.findallBttn);
+            this.Controls.Add(this.negativeBttn);
+            this.Controls.Add(this.integerListBox);
+            this.Name = "Form1";
+            this.Text = "Integer List";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox integerListBox;
+        private System.Windows.Forms.Button negativeBttn;
+        private System.Windows.Forms.Button findallBttn;
+    }
+}
+
---
+++ week 7/aarons-wood_13_1/aarons-wood_13_1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aarons_wood_13_1
+{
+    public partial class Form1 : Form
+    {
+        private List<int> integers = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadIntegersFromFile();
+        }
+
+        private void LoadIntegersFromFile()
+        {
+            // clear the list and listbox to avoid duplicates
+            integers.Clear();
+            integerListBox.Items.Clear();
+
+            // read integers from file and add to list
+            try
+            {
+                using (StreamReader sr = new StreamReader("random.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (int.TryParse(line, out int number))
+                        {
+                            integers.Add(number);
+                            integerListBox.Items.Add(number); 
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error reading file: {ex.Message}");
+            }
+        }
+
+        private void negativeBttn_Click(object sender, EventArgs e)
+        {
+            // remove negative numbers
+            for (int i = integerListBox.Items.Count - 1; i >= 0; i--)
+            {
+                if (int.TryParse(integerListBox.Items[i].ToString(), out int number) && number < 0)
+                {
+                    integerListBox.Items.RemoveAt(i); 
+                }
+            }
+
+            integers.RemoveAll(n => n < 0);
+        }
+
+        private void findallBttn_Click(object sender, EventArgs e)
+        {
+            // clear and display only numbers in the range 1 through 10
+            integerListBox.Items.Clear();
+            foreach (int num in integers.FindAll(n => n >= 1 && n <= 10))
+            {
+                integerListBox.Items.Add(num);
+            }
+        }
+    }
+}
